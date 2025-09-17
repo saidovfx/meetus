5e349d30af64823f8c1e0e11e637f9b6998fa186
saidovfx <muhammadalisaidov007@gmail.com>
2025-09-17 07:09:02 +0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, getUsers } = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware'); // keyin yozamiz
+const authMiddleware = require('../middleware/authMiddleware'); 
 
-// Ro‘yxatdan o‘tish
 router.post('/register', register);
 
-// Login
+
 router.post('/login', login);
 
 router.get('/users', authMiddleware, getUsers);
