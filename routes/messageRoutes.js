b6cd08ca5c20af185449aabd24e5eeabf8244424
saidovfx <muhammadalisaidov007@gmail.com>
2025-09-17 07:09:20 +0500

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,14 +3,12 @@ const router = express.Router();
 const multer = require('multer');
 const { sendMessage, getMessages } = require('../controllers/messageController');
 
-// Multer sozlamalari
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// Xabar yuborish - media nomli fayl uchun multer middleware qo‘shildi
 router.post('/send', upload.single('media'), sendMessage);
 
-// Ikkala user orasidagi xabarlarni olish
 router.get('/:userId1/:userId2', getMessages);
 
 module.exports = router;
