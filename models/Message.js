cc25365fc99130ab35d9f94f14422f4a340e076c
saidovfx <muhammadalisaidov007@gmail.com>
2025-09-17 07:08:29 +0500

    Update Message.js
    This is my firsh message app so this the message model

--- models/Message.js
+++ models/Message.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, default: '' },  // text bo‘lmasa bo‘ladi
+  text: { type: String, default: '' },  
   mediaUrl: { type: String, default: null }, 
   mediaType: { type: String, default: null },
   createdAt: { type: Date, default: Date.now }
